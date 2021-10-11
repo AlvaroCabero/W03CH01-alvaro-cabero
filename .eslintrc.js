74b8e00d02249993f6dc20d9cd57adace8fab5bb
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-11 20:42:50 +0200

    :construction_worker: Added eslint and babel configuration and favicon : )

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "import/extensions": "off",
+    "lines-between-class-members": "off",
+    "no-console": "off",
+  },
 };
